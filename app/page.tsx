0d835492a165a27938efd982c235d5cc5aea9ecf
chank <galbii@proton.me>
2025-05-10 20:33:57 -0700

    sick bro

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from '@/app/_home/hero'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Oracle Club Pro - Transform Your Business',
+  title: 'orcaclub.pro | redesign your workflow.',
   description: 'Streamline your operations, boost productivity, and drive growth with our comprehensive business management solution.',
 }
 
